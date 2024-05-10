1900677c19be84663a614297814d1a9eda3e6246
Mohit Singh Rawat <94630639+mohitSr1021@users.noreply.github.com>
2024-05-10 18:46:39 +0530

    updated resume

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,7 +1,7 @@
-import { useDarkMode } from "./DarkModeContext";
 import "../styles/Resume.css";
+import { useDarkMode } from "./DarkModeContext";
 import { Link } from "react-router-dom";
-import myResume from "../assets/pdf/Resume0_0.pdf";
+import myResume from "../assets/pdf/resume.pdf";
 
 function Resume() {
   const { isDarkMode } = useDarkMode();
@@ -9,7 +9,12 @@ function Resume() {
   return (
     <>
       <div className={` container _Resume ${isDarkMode ? "dark-mode" : ""}`}>
-        <Link to={myResume} target="_blank" download="Resume0_0.pdf" rel="noopener">
+        <Link
+          to={myResume}
+          target="_blank"
+          download="Resume0_0.pdf"
+          rel="noopener"
+        >
           <h3 style={{ color: isDarkMode ? "white" : "red" }}>
             Click me to Download
           </h3>
